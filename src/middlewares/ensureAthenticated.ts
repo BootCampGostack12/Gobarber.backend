dafc748cd9419f2808f4a8ffbc7214d481d3c85d
mpgxc <mpgx5.c@gmail.com>
2020-08-28 04:32:53 -0300

    feeat: add auth jwt on routes

---
+++ src/middlewares/ensureAthenticated.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+export default function ensureAthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+): void {
+    const authheader = request.headers.authorization;
+
+    if (!authheader) {
+        throw new Error('JWT Token is missing!');
+    }
+
+    const [, token] = authheader.split(' ');
+    try {
+        const isValidToken = verify(token, authConfig.jwt.secret);
+        const { sub } = isValidToken as TokenPayload;
+
+        request.user = {
+            id: sub,
+        };
+
+        return next();
+    } catch (error) {
+        throw new Error('Invalid JWT Token!');
+    }
+}
