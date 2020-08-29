fd222748ee6dd0975b3fb78715cc1f375c8a1df8
mpgxc <mpgx5.c@gmail.com>
2020-08-29 05:01:01 -0300

    refactor:

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
 /* eslint-disable no-console */
-import 'reflect-metadata';
-import express from 'express';
-import routes from './routes';
-import './database';
+import App from './app';
 
-const server = express();
-server.use(express.json());
-server.use(routes);
-
-server.listen(3333, () => {
+App.listen(3333, () => {
     console.log('Server GoStck! Iniciado! 🚀');
 });
