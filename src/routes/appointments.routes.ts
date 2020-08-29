a811d0fb91251492df495b6ef6aff491e77c5c26
mpgxc <mpgx5.c@gmail.com>
2020-08-29 08:16:01 -0300

    fix: not find middleware

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,7 +2,7 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import ensureAthenticated from '../middlewares/ensureAthenticated';
+import ensureAthenticated from '../middlewares/ensureAuthenticated';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
